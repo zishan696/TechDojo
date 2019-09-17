cc0ecf4276f6b8aadd5dbe39eba557571df815e7
zishan696 <dantex696@outlook.com>
2019-09-18 00:13:50 +0600

    Task 8: part 2

--- Techdojo-master/src/App.js
+++ Techdojo-master/src/App.js
@@ -254,15 +254,19 @@ class Page extends React.Component {
   }
 }
 
+function ListItem(props) {
+  return <li>{props.value}</li>;
+}
+
 function NumberList(props) {
   const numbers = props.numbers;
-  const listItems = numbers.map((number) =>
-  <li key={number.toString()}>
-  {number}
-</li>
-  );
   return (
-    <ul>{listItems}</ul>
+    <ul>
+      {numbers.map((number) =>
+        <ListItem key={number.toString()}
+                  value={number} />
+      )}
+    </ul>
   );
 }
 
