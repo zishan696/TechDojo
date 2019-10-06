a80032936f8fee6cd000f95a4b41d0e67ee251dc
zishan696 <dantex696@outlook.com>
2019-10-06 14:38:13 +0600

    React native: Passing parameters to routes

--- AwesomeProject2/App.js
+++ AwesomeProject2/App.js
@@ -10,30 +10,38 @@ class HomeScreen extends React.Component {
         <Text>Home Screen</Text>
         <Button
           title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
+          onPress={() => {
+            this.props.navigation.navigate('Details', {
+              itemId: 86,
+              otherParam: 'anything you want here',
+            });
+          }}
         />
       </View>
     );
   }
 }
 
-
 class DetailsScreen extends React.Component {
   render() {
+    const { navigation } = this.props;
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Details Screen</Text>
+        <Text>
+          itemId: {JSON.stringify(navigation.getParam('itemId', 'NO-ID'))}
+        </Text>
+        <Text>
+          otherParam:
+          {JSON.stringify(navigation.getParam('otherParam', 'default value'))}
+        </Text>
         <Button
           title="Go to Details... again"
-          onPress={() => this.props.navigation.push('Details')}
-        />
-        <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.goBack()}
+          onPress={() =>
+            navigation.push('Details', {
+              itemId: Math.floor(Math.random() * 100),
+            })
+          }
         />
       </View>
     );
